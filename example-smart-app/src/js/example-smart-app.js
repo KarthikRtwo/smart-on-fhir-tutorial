c9ca7fcff681a8ee28fdcb8bfb660b2d298c25a1
KarthikRtwo <136821737+KarthikRtwo@users.noreply.github.com>
2024-04-13 10:55:40 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -22,9 +22,13 @@
                     }
                   });
 
+        var medication = smart.patient.api.fetchAll({
+          type: 'MedicationOrder'
+        });
+
         $.when(pt, obv).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt, obv).done(function(patient, obv,medication) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
@@ -60,6 +64,13 @@
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
 
+          //Medications
+          var medications = medication.map(function(med) {
+            return med.medicationCodeableConcept.text;
+          });
+          console.log('Medications:', medications);
+          p.medications = medications.join(', ');
+          console.log('Final patient data:', p);
           ret.resolve(p);
         });
       } else {
